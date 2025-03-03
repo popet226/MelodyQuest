bbf2fbbe621851177c7c3501dacc1265bcf3bd9f
Tigiev Daniil <ditigiev@edu.hse.ru>
2025-03-03 17:18:04 +0300

    added changes to the bot logic

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,27 +1,63 @@
-import telebot
 from dotenv import load_dotenv
+import django
 import os
+import telebot
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_searcher.settings')
+django.setup()
+
+from searcher.views import search_song
 
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
+# Флаг для отслеживания состояния работы бота
+is_bot_active = False
+
+
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    bot.reply_to(message, 'Привет! Я твой бот по поиску ссылок на скачивание музыки!\nЧтобы скачать песню, введи /search\nЧтобы узнать, что умеет бот, введи /help или /about')
-    
+    global is_bot_active
+    if not is_bot_active:
+        bot.reply_to(message, 'Привет! Я твой бот по поиску музыки. Чтобы начать поиск, введи /search.\n Чтобы узнать, что умеет бот, введи /help или /about')
+        is_bot_active = True
+    else:
+        bot.reply_to(message, 'Бот уже активен. Чтобы остановить, введи /stop.')
+
+@bot.message_handler(commands=['stop'])
+def stop_command(message):
+    global is_bot_active
+    if is_bot_active:
+        bot.reply_to(message, 'Бот завершил свою работу. До свидания!')
+        is_bot_active = False
+    else:
+        bot.reply_to(message, 'Бот не активен. Введи /start для начала.')
+
+
 @bot.message_handler(commands=['help', 'about'])
 def help_command(message):
     bot.reply_to(message, 'Раздел помощи. Информация о боте')
 
 @bot.message_handler(commands=['search'])
 def search_command(message):
-    bot.reply_to(message, 'Какую песню хочешь скачать?')
-
+    if is_bot_active:
+        bot.reply_to(message, 'Какую песню хочешь скачать?')
+    else:
+        bot.reply_to(message, 'Для начала работы с ботом введите /start')
+        
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    response = f"Ищем песню {message.text}"
-    bot.send_message(message.chat.id, response)
+    if is_bot_active:
+        response = f"Ищем песню: {message.text}"
+        bot.send_message(message.chat.id, response)
+    else:
+        bot.reply_to(message, 'Для начала работы с ботом введите /start')
+    
+def run_bot():
+    """Запускает Telegram-бота"""
+    bot.polling(none_stop=True)
 
-bot.polling()
+if __name__ == "__main__":
+    run_bot()
